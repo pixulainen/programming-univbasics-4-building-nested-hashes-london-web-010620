9514ed6d224cc100610bfd3a01d32d7be6d376a1
George Apetrei <george.apetrei@gmail.com>
2019-12-17 15:31:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,12 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	{:railroads=> {
-  	:rent_in_dollars => 4
+  	:rent_in_dollars => {
+  	  :one
+  	  :two
+  	  :three 
+  	  :four
+  	}
   	}	
   	  
   	}
