42c1e1376b74d81755060f047eee3161aee861e9
George Apetrei <george.apetrei@gmail.com>
2019-12-17 15:56:21 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,12 +26,14 @@ def monopoly_with_third_tier
   # complexity.
 	{:railroads => {
   	:rent_in_dollars => {
+  	  :names =>	{
   	  :one_piece_owned => 25,
   	  :two_piece_owned => 50,
   	  :three_pieces_owned =>100,
   	  :four_pieces_owned => 200
   	}
-  	:names => 4
+  	}
+  	
   	}	
   	  
   	}
